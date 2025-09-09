665ea6dd78af1e0774c9f6e6dcb4d6edc3ce8eb1
emilie <em.derian@laposte.net>
2025-09-09 14:05:10 +0200

    MAJ

--- SupercoursVue/src/App.vue
+++ SupercoursVue/src/App.vue
@@ -5,37 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 
 <template>
-  <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>{{ msg }}</p>
-    <p>Magic number is {{ magicNumber }} and Pi is {{ pi }}</p>
-    <p>User object: Name is {{ object.name }} and Age is {{ object.age }}</p>
-    <p>{{ myFunction('test') }}</p>
-
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+  <h1>ici c'est l'accueil ! ✈️</h1>
 </template>
 
 <script setup lang="js">
-
-const msg = 'Welcome !!';
-const magicNumber = 42;
-const pi = 3.14;
-const object = { name: 'John', age: 30 };
-const myFunction = (string) => {
-  return string;
-}
-
+import { ref } from 'vue';
 </script>
 
 
---
+++ SupercoursVue/src/views/exercices/exercice-text-interpolation.vue
@@ -0,0 +1,52 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'  
+</script> -->
+
+
+
+<template>
+  <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>{{ msg }}</p>
+    <p>Magic number is {{ magicNumber }} and Pi is {{ pi }}</p>
+    <p>User object: Name is {{ object.name }} and Age is {{ object.age }}</p>
+    <p>{{ myFunction('test') }}</p>
+    <p>{{ bool() }}</p>
+
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup lang="js">
+
+import { ref } from 'vue';
+
+const msg = 'Welcome !!';
+const magicNumber = 42;
+const pi = 3.14;
+const object = { name: 'John', age: 30 };
+const myFunction = (string) => {
+  return string;
+}
+function bool() {
+  if(Math.random() > 0.5){
+    return 'livre boba fett';
+  } else {
+    return 'livre de la jungle';
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
